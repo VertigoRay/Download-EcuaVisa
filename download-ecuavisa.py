3288eefa41a38d89f243a50572a9eb9be31c75ba
Raymond Piller <VertigoRay@users.noreply.github.com>
2016-03-01 02:11:04 -0500

    Update download-ecuavisa.py

--- download-ecuavisa.py
+++ download-ecuavisa.py
@@ -1,19 +1,27 @@
 #!/usr/bin/python3
 
 plex = '/mnt/plex'
-plex_tv_ecuavisa = '%s/Media/TV/EcuaVisa' % plex
-plex_media_scanner = '%s/Media/TV/EcuaVisa' % plex
+plex_media_scanner = '/usr/lib/plexmediaserver/Plex Media Scanner'
+plex_media_scanner_section = '1'
+plex_LD_LIBRARY_PATH = '/usr/lib/plexmediaserver'
 
-noticieros_anteriores = 'http://www.ecuavisa.com/noticieros-anteriores'
-noticieros_episode = 'http://www.ecuavisa.com/ajax-noticiero/nojs/%s/ampliado'
-number_of_episodes_to_keep = 12
+plex_tv_ecuavisa = '%s/Media/TV/EcuaVisa' % plex
+plex_tv_ecuavisa_televistazo = '%s/Televistazo' % plex_tv_ecuavisa
+plex_tv_ecuavisa_telemundo = '%s/Telemundo' % plex_tv_ecuavisa
+
+televistazo = 'http://www.dailymotion.com/rss/user/ecuavisa/lang/es/search/Televistazo/1'
+telemundo = 'http://www.dailymotion.com/rss/user/ecuavisa/lang/es/search/Telemundo/1'
+
+number_of_televistazo_episodes_to_keep = 6
+number_of_telemundo_episodes_to_keep = 6
 
 ################################################
 
-import datetime, io, os, re, subprocess, sys, time, traceback, urllib.request
+import datetime, feedparser, io, os, re, subprocess, sys, time, traceback
 import you_get, you_get.common, you_get.extractors
 
 from contextlib import redirect_stdout
+from pathlib import Path
 from stat import S_ISREG, ST_CTIME, ST_MODE
 from subprocess import call
 
@@ -27,62 +35,46 @@ def logger(msg, log='sys', level='info', event_id='0x11800000'):
     print('[EcuaVisa]%s: %s' % (level, msg))
     # call(["synologset1", log, level, event_id, ('[EcuaVisa] %s' % msg)])
 
-def month_str_to_int(month):
-    if type(month) is int:
-        return month
+def cleanup(number_of_episodes_to_keep, dirpath=r'.'):
+    # get all entries in the directory w/ stats
+    entries = (os.path.join(dirpath, fn) for fn in os.listdir(dirpath))
+    entries = ((os.stat(path), path) for path in entries)
 
-    months = [
-        'enero',
-        'febrero',
-        'marzo',
-        'abril',
-        'mayo',
-        'junio',
-        'julio',
-        'agosto',
-        'septiembre',
-        'octubre',
-        'noviembre',
-        'diciembre',
-    ]
+    # leave only regular files, insert creation date
+    entries = ((stat[ST_CTIME], path)
+               for stat, path in entries if S_ISREG(stat[ST_MODE]))
+    #NOTE: on Windows `ST_CTIME` is a creation date 
+    #  but on Unix it could be something else
+    #NOTE: use `ST_MTIME` to sort by a modification date
 
-    return months.index(month) + 1
-
-logger('Checking for New Episodes ...')
-req = urllib.request.Request(noticieros_anteriores)
-try:
-    resp = urllib.request.urlopen(req)
-except urllib.error.HTTPError as e:
-    logger('HTTPError: %s' % e)
+    downloaded_episodes = list()
+    for cdate, path in sorted(entries, reverse=True):
+        downloaded_episodes.append(path)
 
 
-respData = resp.read()
+    for i in list(range(number_of_episodes_to_keep, 999)):
+        try:
+            rm_this_file = downloaded_episodes[i]
+        except IndexError:
+            logger('Done looking for Old Episodes!')
+            break
 
-episode_numbers = sorted(list(map(int, set(re.findall(r'ajax\-noticiero\/nojs\/([^\/]+)\/ampliado',str(respData))))), reverse=True)
-logger('Found %s Episodes.' % len(episode_numbers))
+        logger('Deleting Old Episode: %s' % rm_this_file)
 
-os.chdir(plex_tv_ecuavisa)
-downloaded_something = False
+        try:
+            os.remove(rm_this_file)
+        except OSError:
+            logger('Episode found a second ago, no longer exists. Moving on ...', level='warn')
+            pass
 
-for i in list(range(number_of_episodes_to_keep)):
-    req = urllib.request.Request(noticieros_episode % episode_numbers[i])
-    try:
-        resp = urllib.request.urlopen(req)
-    except urllib.error.HTTPError as e:
-        logger('HTTPError: %s' % e)
-        continue
 
-    respData = resp.read()
-
-    url = (re.findall(r'<iframe(?:[^>]*)src="([^"]+)"',str(respData)))[0]
-    logger('Downloading: %s' % url)
-
-    # url = 'http://www.dailymotion.com/embed/video/x3qf80s?syndication=173592?inf  o=0&logo=0&related=0&'
+def download(url):
+    # url = 'http://www.dailymotion.com/embed/video/x3qf80s'
     dm_info = {}
     with io.StringIO() as buf, redirect_stdout(buf):
         you_get.extractors.dailymotion.dailymotion_download(url, info_only=True)
         output = buf.getvalue().split('\n')
-
+    #        
     for line in output:
         try:
             m = re.findall(r'(\w+):\s+(.+)', line)[0]
@@ -94,52 +86,115 @@ for i in list(range(number_of_episodes_to_keep)):
 
     ext = re.findall(r'\(video\/(\w+)\)', dm_info['Type'])[0]
     m = re.findall(r'(\w+)', dm_info['Title'])
-    d = datetime.datetime(int(m[-1]), int(month_str_to_int(m[-2])), int(m[-3]))
-    you_get.common.output_filename = 'EcuaVisa - '+ "{:%Y-%m-%d}".format(d) +' - '+ ' '.join(m[:-3]) +'.'+ ext
+    
+    title = ' '.join(m[:-3])
+
+    if m[-3] == 'Telemundo':
+        d = datetime.datetime(datetime.datetime.now().year, int(month_str_to_int(m[-1])), int(m[-2]))
+        title = ' '.join(m[:-2])
+    elif m[-2] == 'de':
+        d = datetime.datetime(datetime.datetime.now().year, int(month_str_to_int(m[-1])), int(m[-3]))
+    else:
+        try:
+            d = datetime.datetime(int(m[-1]), int(month_str_to_int(m[-2])), int(m[-3]))
+        except ValueError:
+            d = datetime.datetime(int(m[-1]), int(month_str_to_int(m[-3])), int(m[-2]))
+
+    you_get.common.output_filename = 'EcuaVisa - '+ "{:%Y-%m-%d}".format(d) +' - '+ title +'.'+ ext
 
     if os.path.isfile(you_get.common.output_filename):
         logger('Skipping Downloaded: %s' % you_get.common.output_filename)
     else:
         try:
             download = you_get.extractors.dailymotion.dailymotion_download(url)
-            downloaded_something = True
             logger('Downloaded: %s' % download)
+            return True
         except:
             logger(traceback.format_exc(), level='err')
-
-dirpath = r'.'
-
-# get all entries in the directory w/ stats
-entries = (os.path.join(dirpath, fn) for fn in os.listdir(dirpath))
-entries = ((os.stat(path), path) for path in entries)
-
-# leave only regular files, insert creation date
-entries = ((stat[ST_CTIME], path)
-           for stat, path in entries if S_ISREG(stat[ST_MODE]))
-#NOTE: on Windows `ST_CTIME` is a creation date 
-#  but on Unix it could be something else
-#NOTE: use `ST_MTIME` to sort by a modification date
-
-downloaded_episodes = list()
-for cdate, path in sorted(entries, reverse=True):
-    downloaded_episodes.append(path)
+            return False
 
 
-for i in list(range(number_of_episodes_to_keep, 999)):
-    try:
-        rm_this_file = downloaded_episodes[i]
-    except IndexError:
-        logger('Done looking for Old Episodes!')
-        break
-
-    logger('Deleting Old Episode: %s' % rm_this_file)
+def month_str_to_int(month):
+    if type(month) is int:
+        return month
 
     try:
-        os.remove(rm_this_file)
-    except OSError:
-        logger('Episode found a second ago, no longer exists. Moving on ...', level='warn')
+        return int(month)
+    except ValueError:
         pass
 
+    months = {
+        'enero': 1,
+        'febrero': 2,
+        'marzo': 3,
+        'abril': 4,
+        'mayo': 5,
+        'junio': 6,
+        'julio': 7,
+        'agosto': 8,
+        'septiembre': 9,
+        'octubre': 10,
+        'noviembre': 11,
+        'diciembre': 12,
+    }
+
+    return months[month]
+
+if os.path.exists('/tmp/ecuavista-downloading'):
+    exit()
+
+Path('/tmp/ecuavista-downloading').touch()
+
+logger('Checking for New Episodes ...')
+feed_televistazo = feedparser.parse( televistazo )
+feed_telemundo = feedparser.parse( telemundo )
+
+downloaded_something = False
+
+try: 
+    os.makedirs(plex_tv_ecuavisa_televistazo)
+except OSError:
+    if not os.path.isdir(plex_tv_ecuavisa_televistazo):
+        raise
+
+os.chdir(plex_tv_ecuavisa_televistazo)
+
+i=0
+for item in feed_televistazo['items']:
+    i+=1
+    downloaded = download(item['media_content'][0]['url'])
+    if downloaded:
+        downloaded_something = True
+
+    if i == number_of_televistazo_episodes_to_keep:
+        break
+
+cleanup(number_of_televistazo_episodes_to_keep)
+
+
+try: 
+    os.makedirs(plex_tv_ecuavisa_telemundo)
+except OSError:
+    if not os.path.isdir(plex_tv_ecuavisa_telemundo):
+        raise
+
+os.chdir(plex_tv_ecuavisa_telemundo)
+i=0
+for item in feed_telemundo['items']:
+    i+=1
+    downloaded = download(item['media_content'][0]['url'])
+    if downloaded:
+        downloaded_something = True
+
+    if i == number_of_telemundo_episodes_to_keep:
+        break
+
+cleanup(number_of_telemundo_episodes_to_keep)
+
+
 if downloaded_something:
-    os.environ['LD_LIBRARY_PATH'] = '/usr/lib/plexmediaserver'
-    subprocess.check_call(['/usr/lib/plexmediaserver/Plex Media Scanner', '--scan', '--refresh', '--section', '1'])
+    os.environ['LD_LIBRARY_PATH'] = plex_LD_LIBRARY_PATH
+    subprocess.check_call([plex_media_scanner, '--scan', '--refresh', '--section', plex_media_scanner_section])
+
+
+os.remove('/tmp/ecuavista-downloading')
